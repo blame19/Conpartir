b8b701917e54699d2a044e1c696fcfe35a5d9824
grzegorzbrze <italiano.tt@libero.it>
2015-11-17 17:10:51 +0100

    Create table driver and post

--- MySQL/conpartirSchema.sql
+++ MySQL/conpartirSchema.sql
@@ -7,7 +7,7 @@
 #create database if not exists conpartir;
 use conpartir;
 ###############################################################################
-# Next we create a table clients with parameters 
+# Next we create a table clients with parameters
 # (name, surname, gender, age, email, password, fotoURL)
 ###############################################################################
 DROP TABLE IF EXISTS clients;
@@ -20,5 +20,25 @@ age int not null,
 email varchar(30) not null,
 pass varchar(16) not null,
 fotoURL varchar(50)
+)engine=innodb;
+###############################################################################
+# This table describes driver of car. A driver must insert name and year of car
+###############################################################################
+DROP TABLE IF EXISTS driver;
+create table driver(
+driver_id INT auto_increment,
+client_id int references clients(client_id),
+car_name varchar(15) not null,
+car_year int not null,
+primary key (driver_id, client_id)
+)engine=innodb;
+###############################################################################
+# This table describes post.
+###############################################################################
+drop table if exists post;
+create table post(
+owner_id int,
+client_id int references clients(client_id),
+primary key (owner_id, client_id)
 )engine=innodb;
 ###############################################################################
