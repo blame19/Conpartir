612dc8abf6c928df6d5e50e41b008d8034e19d6f
grzegorzbrze <italiano.tt@libero.it>
2015-11-17 14:36:59 +0100

    Create a script to use for add or modify rows of database

---
+++ MySQL/conpartirPopolation.sql
@@ -0,0 +1,7 @@
+################################################################################
+# This script is used to add or modify rows of database compartir
+################################################################################
+insert into clients(name, surname, gender, age, email, pass) values
+('Mario', 'Rossi', 'male', 28, 'mario.rossi@gmail.com', 'rossi'),
+('Giulia', 'Verdi', 'female', 24, 'giulia91@libero.it', 'giulia' );
+################################################################################
