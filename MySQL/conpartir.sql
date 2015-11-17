82ffa615cc503852a0facded9fa2f38feea73b8a
grzegorzbrze <italiano.tt@libero.it>
2015-11-17 13:04:17 +0100

    Create MySQL folder for sql scripts and create script conpartir.sql to use for define a schema of DB

---
+++ MySQL/conpartir.sql
@@ -0,0 +1,24 @@
+﻿#######################################################################################################
+# This script define schema of conpartir project
+#######################################################################################################
+# At first we create a database and we start to use it
+#######################################################################################################
+#drop database if exists conpartir;
+#create database if not exists conpartir;
+#use conpartir;
+#######################################################################################################
+# Next we create a table clients with parameters (name, surname, gender, age, email, password, fotoURL)
+#######################################################################################################
+DROP TABLE IF EXISTS clients;
+create table clients(
+client_id INT PRIMARY KEY auto_increment,
+name varchar(15),
+surname varchar(15),
+gender enum('male','female'),
+age int,
+email varchar(30),
+pass varchar(16),
+fotoURL varchar(50)
+)engine=innodb;
+#######################################################################################################
+insert into
