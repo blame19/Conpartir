61c225c0a5035f4a5a2187b0704819fb3de5e833
SergioLazaro94 <sergiozgz1994@gmail.com>
2015-11-17 12:42:30 +0100

    Config files added

---
+++ Conpartir-war/build/web/js/homeCtrl.js
@@ -0,0 +1,57 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module("myapp", ['ngRoute'])
+  
+  .config(function($routeProvider){
+  $routeProvider.when("/",
+    {
+      templateUrl: "pages/home.html",
+      controller: "MainController"
+     // controllerAs: "app"
+    });
+    
+    $routeProvider.when("/about",
+    {
+      templateUrl: "pages/about.html",
+      controller: "MainController"
+     // controllerAs: "app"
+    });
+    $routeProvider.when("/taxis",{
+        templateUrl: "pages/taxis.html",
+        controller: "MainController"
+        // controllerAs: "app"
+    });
+    $routeProvider.when("/cars",{
+        templateUrl: "pages/cars.html",
+        controller: "MainController"
+        // controllerAs: "app"
+    });
+    $routeProvider.when("/signup",{
+        templateUrl: "pages/signup.html",
+        controller: "MainController"
+        // controllerAs: "app"
+    });
+    $routeProvider.when("/login",{
+        templateUrl: "pages/login.html",
+        controller: "MainController"
+        // controllerAs: "app"
+    });
+    $routeProvider.when("/createpost",{
+        templateUrl: "pages/createpost.html",
+        controller: "MainController"
+        // controllerAs: "app"
+    });
+})
+     .controller("MainController", function($scope) {
+         
+      $scope.hello = "Powered by AngularJs";
+      $scope.hasFooter = true;
+      $scope.cities = ["Bologna", "Firenze", "Genova", "Milano", "Napoli", 
+          "Padova", "Pisa", "Roma", "Siena", "Torino", "Venezia", "Verona"];
+   });
+ 
+   
